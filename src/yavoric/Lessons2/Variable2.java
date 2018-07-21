e2337776ffd35e9674abc90197040cee58ab925e
Yavoric <yavoric@gmail.com>
2018-07-21 16:25:17 +0300

    FinalVersion

---
+++ src/yavoric/Lessons2/Variable2.java
@@ -0,0 +1,10 @@
+package yavoric.Lessons2;
+
+public class Variable2 {
+    public static int a;
+    public static int b;
+    public static int c;
+    public static int maxnumber;
+    public static int minnumber;
+    public static double midnumber;
+}
