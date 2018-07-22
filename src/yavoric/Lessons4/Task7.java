86aa8336c0253a8857da003ffe9cb916f049c9f9
Yavoric <yavoric@gmail.com>
2018-07-22 21:14:24 +0300

    FinalVersion

--- src/yavoric/Lessons4/Task7.java
+++ src/yavoric/Lessons4/Task7.java
@@ -3,38 +3,38 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.*;
 public class Task7 {
+    private static int CODE_SUCCESS = 1;
+    private  static int CODE_FAIL = 0;
     public static void main(String[] args) {
         String mail = inputMail();
-        int i = checkEmail();
-        outputMail (int, String);
+        int code = checkEmail(mail);
+        outputMail (mail, code);
 
     }
-    //Функция ввода почты
+   //Функция ввода почты
     public static String inputMail(){
         System.out.println("ведите @-mail:");
         String str = new Scanner(System.in).nextLine();
         return str;
     }
     //Функция проверки почты
-    public static int checkEmail(String Mail1){
-        final Pattern pattern = Pattern.compile("^[A-Za-z.%+\\-]+@[A-Za-z.\\-]+\\.[A-Za-z]{2,4}");
-        final Matcher matcher = pattern.matcher(mail1);
+    public static int checkEmail(String mailTarget){
+        final Pattern pattern = Pattern.compile("^[\\w%+\\-]+@[A-Za-z\\-]+\\.[A-Za-z]{2,4}$");
+        final Matcher matcher = pattern.matcher(mailTarget);
         if (matcher.find()){
-            i=1;
+            return CODE_SUCCESS;
         }
-        else i=0;
-        return i;
+        else
+            return CODE_FAIL;
     }
     //Функция вывода результата
-    public static void outputMail(int){
-        if (i== 1) {
+   public static void outputMail(String mail, int code){
+        if (code== CODE_SUCCESS) {
             System.out.println(mail + ": = Correct");
-        }
-        elese System.out.println(mail+ ": = Incorrect");
+        } else
+            System.out.println(mail+ ": = Incorrect");
     }
 
-}
 
-public class Task7 {
 
 }
