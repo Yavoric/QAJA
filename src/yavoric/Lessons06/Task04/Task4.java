91b3619a561f882440c800fbb7285a25c7d2333b
Yavoric <yavoric@gmail.com>
2018-07-24 22:02:07 +0300

    Version02

--- src/yavoric/Lessons06/Task04/Task4.java
+++ src/yavoric/Lessons06/Task04/Task4.java
@@ -1,7 +1,21 @@
 package yavoric.Lessons06.Task04;
+/*Реализовать класс Abonent, который имеет поля:
+		long id; // необязательное поле, по умолчанию -1
+		String firstName;
+		String lastName;
+		int age;
+		char gender; // 'm' - male, 'f' - female
 
-//import static sun.misc.Version.print;
+	Все поля должны быть помечены модификатором private
+		- реализовать доступ к полям через методы set и get
+		- реализовать возможность создания класса через два конструктора:
+			1-> все поля, без обязательного поля "id"
+			2-> полный конструктор со всем полями
 
+	- реализовать метод void print(); вывода информации абонента в отформатированном виде
+	- реализовать метод boolean isEquals(Abonent abonent); // вернут true, если id обоих объектов равны и не равны -1, false - в остальных случаях
+	- протестировать метод isEquals
+	*/
 public class Task4 {
     public static void main(String[] args) {
         Abonent abb1 = new Abonent("Иванов", "Иван", 23, "M" );
@@ -13,8 +27,5 @@ public class Task4 {
         System.out.println(abb1.isEqual(abb2));
         System.out.println(abb1.isEqual(abb3));
         System.out.println(abb2.isEqual(abb3));
-
-
-
     }
 }
