34a4c9bf994cc75404f7627b8417f38ce99aa211
Yavoric <yavoric@gmail.com>
2018-07-26 23:37:59 +0300

    Version02

---
+++ src/yavoric/Lessons06/Button.java
@@ -0,0 +1,31 @@
+package yavoric.Lessons06;
+
+public class Button {
+    private int height;
+    private int weidht;
+    private String text;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeidht() {
+        return weidht;
+    }
+
+    public void setWeidht(int weidht) {
+        this.weidht = weidht;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
